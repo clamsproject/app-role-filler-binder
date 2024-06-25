99b8ba9bfaa900691580ac6a9121d7d998f4e925
Wesley Ricketts <wricketts@brandeis.edu>
2024-06-25 15:29:49 -0400

    Update app.py and metadata.py - Add timepoint label map preset to app runtime params, replacing hard-coded timepoint labels - Prevent creation of empty annotations when no role-filler pairs are found in an ocr textdocument - add more debug messages

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ import logging
 
 from clams import ClamsApp, Restifier
 from mmif import Mmif, View, Annotation, Document, AnnotationTypes, DocumentTypes
+from mmif.utils import sequence_helper as sqh
 
 # For an NLP tool we need to import the LAPPS vocabulary items
 from lapps.discriminators import Uri
+
+import metadata
 
 import pandas as pd
 from utils.clean_ocr import clean_ocr
@@ -35,19 +38,28 @@ class RoleFillerBinder(ClamsApp):
         self.logger.debug(f"Parameters: {parameters}")
         if not isinstance(mmif, Mmif):
             mmif = Mmif(mmif)
-        views2alignments = mmif.get_alignments(AnnotationTypes.TimePoint, DocumentTypes.TextDocument)
+
         rfb_view = mmif.new_view()
         self.sign_view(rfb_view, parameters)
         rfb_view.new_contain(DocumentTypes.TextDocument)
         rfb_view.new_contain(AnnotationTypes.Alignment)
+
+        label_map = metadata.labelMapPresets.get(parameters['labelMapPreset'])
+        label_map = dict([lm.split(':') for lm in label_map])
+        self.logger.debug(f"Label map: {label_map}")
+
+        views2alignments = mmif.get_alignments(AnnotationTypes.TimePoint, DocumentTypes.TextDocument)
+
         for view_id in views2alignments:
             for alignment in views2alignments[view_id]:
                 source = alignment.get("source")
                 target = alignment.get("target")
+                # ensure that these are the long_id format
                 if alignment.id_delimiter not in source:
                     source = alignment.id_delimiter.join((view_id, source))
                 if alignment.id_delimiter not in target:
                     target = alignment.id_delimiter.join((view_id, target))
+
                 if {mmif[source].at_type, mmif[target].at_type} != {AnnotationTypes.TimePoint,
                                                                     DocumentTypes.TextDocument}:
                     self.logger.debug(f"Skipping unexpected alignment, source: {source}, target: {target}")
@@ -57,26 +69,29 @@ class RoleFillerBinder(ClamsApp):
                     tp, td = mmif[source], mmif[target]
                 else:
                     tp, td = mmif[target], mmif[source]
-                if tp.get("labelset") is None or not {'I', 'Y', 'N', 'C', 'R'}.issubset(set(tp.get("labelset"))):
+                if tp.get("labelset") is None or not set(label_map.keys()).issubset(set(tp.get("labelset"))):
                     self.logger.debug(f"Skipping TimePoint with unexpected labelset: {tp.get('labelset')}.")
                     continue
-                scene_type = tp.get("label")
-                if scene_type in {"I", "Y", "N"}:
-                    ftype = "chyron"
-                elif scene_type in {"C", "R"}:
-                    ftype = "credits"
-                else:
+                scene_type = label_map[tp.get("label")]
+                if scene_type not in {"chyron", "credits"}:
                     self.logger.debug(f"Skipping TimePoint `{tp.long_id}` which has an unsupported scene type:"
                                       f" `{scene_type}`")
                     continue
-                self.logger.debug(f"Processing {ftype.upper()} TextDocument `{td.long_id}`, anchored to TimePoint"
+                self.logger.debug(f"Processing {scene_type.upper()} TextDocument `{td.long_id}`, anchored to TimePoint"
                                   f" `{tp.long_id}`")
                 ocr_text = rf'{td.text_value}'
                 input_seq = " ".join(clean_ocr(ocr_text))
-                parsed = bind_role_fillers(input_seq, ftype)
-                csv_string = pd.DataFrame.from_dict(parsed).to_csv()
-                doc = rfb_view.new_textdocument(text=csv_string)
-                rfb_view.new_annotation(at_type=AnnotationTypes.Alignment, source=alignment.long_id, target=doc.id)
+                parsed = bind_role_fillers(input_seq, scene_type)
+                self.logger.debug(f"Found {len(parsed)} Role-Filler pairs.")
+                if not parsed:
+                    continue
+                else:
+                    csv_string = pd.DataFrame.from_dict(parsed).to_csv()
+                    doc = rfb_view.new_textdocument(text=csv_string)
+                    rfb_view.new_annotation(
+                        at_type=AnnotationTypes.Alignment, source=alignment.long_id, target=doc.long_id
+                    )
+                    self.logger.debug(f"Created annotation {doc.long_id} aligned with {alignment.long_id}")
         return mmif
 
 
