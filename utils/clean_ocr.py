ee77765b0dcbbb458df0a2e70afc71301452106f
Wesley Ricketts <wricketts@brandeis.edu>
2024-07-03 00:32:54 -0400

    add additional regex funcs to clean_ocr.py

--- utils/clean_ocr.py
+++ utils/clean_ocr.py
@@ -2,17 +2,28 @@
 Utility functions for cleaning OCR data.
 """
 from typing import List
-import pandas as pd
+import re
 
 
 def has_alnum(string: str) -> bool:
-    """Returns True if any character in the string is an alphanumeric."""
+    """Returns True if the input string contains any alphanumeric characters."""
     return any(char.isalnum() for char in string)
 
 
 def has_alpha(string: str) -> bool:
-    """Returns True if any character in the string contains alpha characters."""
+    """Returns True if the input string contains any alpha characters."""
     return any(char.isalpha() for char in string)
+
+
+def contains_year(string: str) -> bool:
+    """Returns True if the string contains a valid year from 1000-2999."""
+    return re.search(r"^[12][0-9]{3}$", string) is not None
+
+
+def segment_string(string: str):
+    """Inserts whitespace in a string to segment improperly merged words"""
+    string = re.sub(r"(?<=[A-Za-z][,.])(?=[A-Za-z])", " ", string)
+    return re.sub(r"(?<=[a-z]{3})(?=[A-Z])", " ", string)
 
 
 def clean_ocr(text_document: str) -> List[str]:
@@ -20,15 +31,14 @@ def clean_ocr(text_document: str) -> List[str]:
     allowable_chars = {r'&'}
     cleaned = []
     for line in text_document.split('\n'):
-        if not has_alpha(line):
+        line = line.strip()
+        if not has_alnum(line):
             continue
         else:
-            line = [s for s in line.split() if (len(s) > 1 and has_alpha(s)) or s in allowable_chars]
-        cleaned.extend(line)
+            line = segment_string(line)
+            line = [
+                w for w in line.split() if (len(w) > 1 and has_alpha(w)) or w in allowable_chars or contains_year(w)
+            ]
+        if line:
+            cleaned.extend(line)
     return cleaned
-
-
-if __name__ == '__main__':
-    df = pd.read_csv('doctr-preds.csv', usecols=['textdocument'])
-    df['cleaned'] = df['textdocument'].map(clean_ocr)
-    df.to_csv('cleaned_ocr_chyrons.csv', index=True)
